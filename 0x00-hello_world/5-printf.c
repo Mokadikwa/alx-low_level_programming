acb4c7e83a8c1f8ba28779cce08e9acb3e1ff56d
Mokadikwa <mpmokadikwa@gmail.com>
2023-04-20 14:28:46 -0700

    6 size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include  <stdio.h>
 /**
  * main - A program that prints a line using the print function
- * return: 0 (success)
+ *
+ * return Always 0 (success)
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	return (0);
 }
