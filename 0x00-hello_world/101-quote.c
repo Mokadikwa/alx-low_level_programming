cb8509e3618e0193c57359a80a605102be90ef6e
Mokadikwa <mpmokadikwa@gmail.com>
2023-04-20 16:19:27 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line line to the standard error
- * Return 1 (success)
+ * main - A program that prints a line to the standard error
+ * Return: 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
