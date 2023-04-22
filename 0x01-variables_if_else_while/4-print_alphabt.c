96a5538b1df3e9804baf81ea69ff795491e80a48
Mokadikwa <mpmokadikwa@gmail.com>
2023-04-22 10:30:24 -0700

    alphabet too

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	putchar (c);
+
+	return (0);
+}
