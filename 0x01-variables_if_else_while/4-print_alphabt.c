f1aed10ef2f353041cec4084b9c29f3df2e7372b
Mokadikwa <mpmokadikwa@gmail.com>
2023-04-23 05:24:44 -0700

    alphabet 3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,18 +14,16 @@ int main(void)
 {
 	int n = 97
 
-	while (n <= 122)
+	; while (n <= 122)
 	{
-	if (n == 'q' || n == 'e')
-	{
-	n++;
-	continue;
+		if (n == 'q' || n == 'e')
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
 	}
-
-	putchar(n);
-	n++;
-	}
-
 	putchar('\n');
 	return (0);
 }
