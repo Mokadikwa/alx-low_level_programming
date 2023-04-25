23d16104e6d0fed1178d35cb9f3a8d74b1a8bd75
Mokadikwa <mpmokadikwa@gmail.com>
2023-04-25 12:18:09 -0700

    2 print alph

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10x, in lowercase
+ * Return: Always
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int n;
+
+	n = 0;
+
+	while (n < 9)
+	{
+		alph = 'a';
+		while (alph <= 'z')
+		{
+			_putchar(alph);
+			alph++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
