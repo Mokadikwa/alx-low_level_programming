30d9b74ace55ee93d0b7c1e28f0f892ea8646a31
Mokadikwa <mpmokadikwa@gmail.com>
2023-04-25 05:41:33 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Print _putchar'
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar", '\n');
+
+	return (0);
+}
