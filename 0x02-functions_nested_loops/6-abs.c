87029468e2a873522b3dea351cbb02ec1010acdb
Mokadikwa <mpmokadikwa@gmail.com>
2023-04-26 11:09:06 -0700

    24 hours jack

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ *
+ * @i: input number as an integer
+ *
+ * Returns: absolute value
+ */
+int _abs(int i)
+{
+
+	if (i >= 0)
+	{
+		return (i);
+	}
+		return (-i);
+}
