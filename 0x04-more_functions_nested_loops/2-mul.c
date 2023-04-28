7b8c43369c89e4b4a7fc561a58d8e018337719aa
Mokadikwa <mpmokadikwa@gmail.com>
2023-04-28 12:50:28 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer to miltiply with b
+ * @b: integer to multiply with a
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ * description:' prints the numbers, from 0 to 9'
+ * Return: results
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	if (a = 0; a < 10; a++);
+	{
+		putchar( a + '0');
+		putchar('\n');
+	}
+}
