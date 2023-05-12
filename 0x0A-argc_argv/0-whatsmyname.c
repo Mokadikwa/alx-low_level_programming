35e601942cb68063dba50e2dc31b39a2b562aa53
Mokadikwa <mpmokadikwa@gmail.com>
2023-05-12 14:32:31 -0700

    0. whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry
+ * @argc: unused integer
+ * @argv: program that prints its name
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
