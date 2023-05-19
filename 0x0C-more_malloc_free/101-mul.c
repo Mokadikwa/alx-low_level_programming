2c12201ef3eb8ff6529db632fce5dd356c454132
Mokadikwa <mpmokadikwa@gmail.com>
2023-05-18 20:00:05 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,18 +3,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * on error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-        return(write(1, &c, 1));
-}
 
 /**
  * print_number - prints an integer.
