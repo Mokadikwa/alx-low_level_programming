8b324ea357508e6350d490c9a62aacf2d1574a08
Mokadikwa <mpmokadikwa@gmail.com>
2023-05-22 09:43:30 -0700

    task 2

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: print programs name file is compiled from
+ * Return: results
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
