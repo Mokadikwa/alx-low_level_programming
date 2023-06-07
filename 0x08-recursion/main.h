73b2d5b78a6ced6dd86001df11c239aa2c5a4480
Mokadikwa <mpmokadikwa@gmail.com>
2023-06-07 09:08:07 -0700

    task 1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,9 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
 
+#include <stdio.h>
+#include <unistd.h>
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
