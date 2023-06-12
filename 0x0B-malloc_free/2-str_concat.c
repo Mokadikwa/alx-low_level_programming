50c6e0108d21270c680763d6c6c13a1175d47a10
Mokadikwa <mpmokadikwa@gmail.com>
2023-06-12 13:06:49 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * str_concat - Write a function that concatenates two strings
+ * @s1: concatenate into char s2
+ * @s2: concatenates s1
+ *
+ * Return: pointer to newly appointed space
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *new_str;
+
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+
+	new_str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(new_str, s1);
+	strcpy(new_str + len1, s2);
+
+	return (new_str);
+	free(new_str);
+}
