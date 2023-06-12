3fbb2bbc041d32b17d0032a90ba70a6ed57942e9
Mokadikwa <mpmokadikwa@gmail.com>
2023-06-12 13:17:24 -0700

    0x0B-malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,15 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *new_str;
+	size_t len1, len2;
 
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
 	new_str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 
