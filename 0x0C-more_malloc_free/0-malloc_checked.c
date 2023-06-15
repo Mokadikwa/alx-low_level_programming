8fd8f8a52d7b335b4f70ce32d2ff48862bbe9e91
Mokadikwa <mpmokadikwa@gmail.com>
2023-06-15 06:48:40 -0700

    malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,20 +5,19 @@
 
 /**
  * malloc_checked -  function that allocates memory using malloc
- * @b: pointer
+ * @b: bytes to allocate
  *
  * Return: results
  */
 
 void *malloc_checked(unsigned int b)
 {
-	int *i;
+	void *i;
 
-	i = malloc(sizeof(int) * b);
+	i = malloc(b);
 
 	if (i == NULL)
-	{
 		exit(98);
-	}
+
 	return (i);
 }
