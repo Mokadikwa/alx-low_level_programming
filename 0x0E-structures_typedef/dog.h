1d0aa1e5a0e2624549f1ea22e230804799cb059d
Mokadikwa <mpmokadikwa@gmail.com>
2023-06-19 13:27:12 -0700

    0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+ * struct dog - a new type struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ *
+ * Description: Define a new type struct dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
