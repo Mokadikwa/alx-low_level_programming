2344daae5a8b0441c469bd357d1063616e6462a2
Mokadikwa <mpmokadikwa@gmail.com>
2023-06-29 14:37:41 -0700

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,9 +20,10 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", current->len, current->str);
-		current = current->next;
-
-		count++;
+	
+	current = current->next;
+	count++;
 	}
+
 	return (count);
 }
