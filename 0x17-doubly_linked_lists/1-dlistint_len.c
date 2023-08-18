1a76efa3d2ae9346e5111b1d429c66d7567edb00
Mokadikwa <mpmokadikwa@gmail.com>
2023-08-18 03:50:56 -0700

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: pointer to linked list to print
+ * Return: number of elements in a linkedd list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t element_count = 0;
+
+	while (h != NULL)
+	{
+		element_count++;
+		h = h->next;
+	}
+	return element_count;
+}
