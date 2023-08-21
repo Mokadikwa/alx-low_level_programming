7faa3740f3df36ef3bc45abac3cc28b5ad541f09
Mokadikwa <mpmokadikwa@gmail.com>
2023-08-21 07:28:49 -0700

    dynamic library task

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+c_files=$(ls *.c 2>/dev/null)
+if [ -z "$c_files" ]; then
+    echo "No .c files found in the current directory."
+    exit 1
+fi
+gcc -shared -o liball.so -fPIC $c_files
+
+if [ $? -eq 0 ]; then
+    echo "Dynamic library liball.so created successfully."
+else
+    echo "Failed to create the dynamic library."
+fi
